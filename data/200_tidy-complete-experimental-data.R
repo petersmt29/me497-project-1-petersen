d48cb5c523b64f4fd88c994ebfbb6936afea47f7
Mary Petersen <petersmt@petersmt-5510.rose-hulman.edu>
2018-09-19 15:10:46 -0400

    starting to add in tables/data

---
+++ data/200_tidy-complete-experimental-data.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+data <- read.csv("data/complete-experimental-data.csv")
+colnames(data) <- c("Flow", "h1", "h2", "Flow", "h1", "h2", "Flow", "h1", "h2")
+units <- data[1,]
+data <- data[-1,]
+
+data_1.5 <- data[,1:3]
+data_2.5 <- data[,4:6]
+data_3.5 <- data[,7:9]
+
+data <- rbind(data_1.5, data_2.5, data_3.5)
+
+data
