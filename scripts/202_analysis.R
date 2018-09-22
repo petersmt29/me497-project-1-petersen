8dac308ae4f4c8127bb04a7d5d078e4ea60d88ba
Mary Petersen <petersmt@petersmt-5510.rose-hulman.edu>
2018-09-22 13:08:18 -0400

    added questions

--- scripts/202_analysis.R
+++ scripts/202_analysis.R
@@ -2,15 +2,22 @@ library(tidyverse)
 
 df <- read_csv("results/tidy-experimental-data.csv")
 constants <- read_csv("data/constants.csv")
+D <- constants$value[1]/1000
+L <- constants$value[2]/100
+rho <- constants$value[3]
+epsilon <- constants$value[4]/1000
+nu <- constants$value[5]/1000
 
-#what is
-V <- 1
-g <- 1
+V <- 94.64e-6
+g <- 9.81
 
-#calc friction factor and reynolds number
 df <- df %>%
-  mutate(fac = (pi^2*constants$value[1]^5*g*(h1-h2))/(8*constants$value[2]*V^2),
-         num = (4*constants$value[3]*V)/(pi*constants$value[5]*constants$value[1]))
+  mutate(flow_si = flow*6.3e-5)
+
+#calc friction factor(fac) and reynolds number(num)
+df <- df %>%
+  mutate(fac = (pi^2*D^5*g*(h1-h2))/(8*L*flow_si^2),
+         num = (4*rho*flow_si)/(pi*nu*D))
 
 #group mean friction factor and reynolds number by flow rate (1.5, 2.5, 3.5)
 dfGrouped <- df %>% group_by(flow) %>% summarise(fac = mean(fac),
