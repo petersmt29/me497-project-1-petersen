67e3723ae3afe3ed37b87f94189153efe918e486
graphdr <graphdoctor@gmail.com>
2018-09-25 16:42:59 -0400

    units on h1 h2

--- scripts/202_analysis.R
+++ scripts/202_analysis.R
@@ -11,12 +11,19 @@ V <- 94.64e-6
 g <- 9.81
 
 df <- df %>%
-  mutate(flow_si = flow*6.3e-5)
+  mutate(flow_si = flow/15852)
+
+
+# Mary, the units of height in the raw data are in mm
+# must divide by 1000 to convert to meters before doing the computation 
 
 #calc friction factor(fac) and reynolds number(num)
 df <- df %>%
+  mutate(h1 = h1/1000, h2 = h2/1000) %>% 
   mutate(fac = (pi^2*D^5*g*(h1-h2))/(8*L*flow_si^2),
          num = (4*rho*flow_si)/(pi*mu*D))
+
+
 
 #group mean friction factor and reynolds number by flow rate (1.5, 2.5, 3.5)
 dfGrouped <- df %>% group_by(flow) %>% summarise(fac = mean(fac),
@@ -27,3 +34,4 @@ dfGrouped <- cbind(c("Low", "Medium", "High"), dfGrouped)
 colnames(dfGrouped) <- c("Flow rate level", "Flow rate (gpm)", "Mean friction factor", "Reynolds number")
 
 write_csv(dfGrouped, "results/mean-fricfac-reynum.csv")
+
